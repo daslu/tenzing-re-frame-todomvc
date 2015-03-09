a74e81d54421a6ae2b4b5756651415ca10058089
Daniel Slutsky <daniel.slutsky@gmail.com>
2015-03-10 00:40:04 +0200

    updated app

--- src/cljs/tenzing_re_frame_todomvc/app.cljs
+++ src/cljs/tenzing_re_frame_todomvc/app.cljs
@@ -1,18 +1,35 @@
 (ns tenzing-re-frame-todomvc.app
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require-macros [secretary.core :refer [defroute]])
+  (:require [goog.events :as events]
+            [reagent.core :as reagent :refer [atom]]
+            [re-frame.core :refer [dispatch dispatch-sync]]
+            [secretary.core :as secretary]
+            [tenzing-re-frame-todomvc.handlers]
+            [tenzing-re-frame-todomvc.subs]
+            [tenzing-re-frame-todomvc.views])
+  (:import [goog History]
+           [goog.history EventType]))
 
-(defn some-component []
-  [:div
-   [:h3 "I am a component!"]
-   [:p.someclass
-    "I have " [:strong "bold"]
-    [:span {:style {:color "red"}} " and red"]
-    " text."]])
+(enable-console-print!)
 
-(defn calling-component []
-  [:div "Parent component"
-   [some-component]])
+;; -- Routing -----------------------------------------------------------------
 
-(defn init []
-  (reagent/render-component [calling-component]
-                            (.getElementById js/document "container")))
+(defroute "/" [] (dispatch [:set-showing :all]))
+(defroute "/:filter" [filter] (dispatch [:set-showing (keyword filter)]))
+
+(def history (History.))
+
+(events/listen history EventType.NAVIGATE
+  (fn [e] (secretary/dispatch! (.-token e))))
+
+(.setEnabled history true)
+
+
+;; -- Entry Point -------------------------------------------------------------
+
+(defn ^:export main
+  []
+  (dispatch-sync [:initialise-db])
+  (reagent/render [tenzing-re-frame-todomvc.views/todo-app]
+                  (.getElementById js/document "app")))
+
